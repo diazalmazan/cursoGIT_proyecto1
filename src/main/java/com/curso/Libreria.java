7ca5bfc66414209a6fb9b0783598ca80d015aacb
Iván Díaz <i.diaz@ibermatica.com>
2021-03-16 15:30:35 +0000

    version decimales

--- src/main/java/com/curso/Libreria.java
+++ src/main/java/com/curso/Libreria.java
@@ -19,4 +19,12 @@ public class Libreria
         return numero1 - numero2;
     }
     
+    public static double suma(double numero1, double numero2){
+        return numero1 + numero2;
+    }
+    
+    public static double resta(double numero1, double numero2){
+        return numero1 - numero2;
+    }
+    
 }
